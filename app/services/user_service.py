e22934cd613a8fbb601869d9a93fee726daa92cf
Moynul Islam <moynul2015@cseku.ac.bd>
2026-04-12 15:09:12 +0600

    implement delete user endpoint

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -57,3 +57,13 @@ class UserService:
         await self.user_repository.db.commit()
 
         return UserRead.model_validate(user)
+
+    async def deactivate_user(self, user_id: UUID) -> None:
+        user = await self.user_repository.get_active_by_id(user_id)
+
+        if user is None:
+            raise NotFoundException(ErrorMessages.USER_NOT_FOUND)
+
+        user.is_deleted = True
+
+        await self.user_repository.db.commit()
