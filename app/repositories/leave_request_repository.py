425e63807553bde1204a49de1637c854a1ddc2ff
Moynul Islam <moynul2015@cseku.ac.bd>
2026-04-13 13:40:06 +0600

    implement get leaves with user endpoint

--- app/repositories/leave_request_repository.py
+++ app/repositories/leave_request_repository.py
@@ -1,3 +1,6 @@
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
 from app.models import LeaveRequest
 from app.repositories.base_repository import EntityBaseRepository
 
@@ -5,3 +8,13 @@ from app.repositories.base_repository import EntityBaseRepository
 class LeaveRequestRepository(EntityBaseRepository[LeaveRequest]):
     def __init__(self, db):
         super().__init__(db, LeaveRequest)
+
+    async def get_all_active_with_users(self) -> list[LeaveRequest]:
+        statement = (
+            select(self.model)
+            .options(selectinload(self.model.user))
+            .where(self.model.is_deleted.is_(False))
+        )
+        result = await self.db.execute(statement)
+
+        return list(result.scalars().all())
